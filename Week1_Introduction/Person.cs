23a11cff5b7df46b79c47f977340baf718d84ce3
Ryan Attard <ryanattard@gmail.com>
2024-10-07 11:08:14 +0200

    Add project files.

---
+++ Week1_Introduction/Person.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week1_Introduction
+{
+
+    //what is a class?
+    //a class is a blueprint/template which defines a "thing" from the real-world by grouping
+    //a set of related components/members/...
+    internal class Person
+    {
+        //properties:
+        //like attributes which best describe the class is about
+        //like containers for the data that will be assigned/ accessed
+
+        //rules to respect when defining a property
+        //1. name is without spaces, symbols, numbers
+        //2. names of properties must be in Pascal casing means that the name starts with a capital letter
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Title { get; set; }
+        public string Surname { get; set; }
+        public string FullName { get; set; }
+
+        //methods
+        //methods are behaviours which the class can have
+        //e.g. a method to validate some property
+        //e.g. a method which regulates the data inside a particular property
+
+        public string CapitalizeFullName()
+        {
+            return FullName.ToUpper();
+        }
+
+
+    }
+}
---
+++ Week1_Introduction/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace Week1_Introduction
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //class is the blueprint; it cannot exist in memory; it doesnt allow assignments
+            //out of the class we can create an object, which will allow us to assign values
+
+            Person p = new Person(); //creating an object
+            p.Name = "Test";
+
+
+            
+
+        }
+    }
+}

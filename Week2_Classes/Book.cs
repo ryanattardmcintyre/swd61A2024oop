b4ef0766d8270bf161aea6799a8a1b3d3683dc66
Ryan Attard <ryanattard@gmail.com>
2024-10-07 12:47:27 +0200

    week 2 commit 1 (properties, encapsulation, method)

---
+++ Week2_Classes/Book.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week2_Classes
+{
+    //enum are predefined sets which you cannot dynamically modify
+    public enum GenreType {Horror=1, Fiction=2, SciFi=3 }
+
+    //a class is  a representation of an entity, which is often referred to as a template
+    public class Book
+    {
+        //Fields:
+        //fields are the containers where data is actually stored
+        private string name;
+        private string author;
+        private bool available;
+
+        //the properties are the characteristics that make up the book
+        //through properties we can GET or SET values
+        //property is like the gateway to access the field via getters and the setters
+        //hence with properties i can control the access/filter the data that goes into or out of the field
+        public string Name
+        {
+            get
+            {
+                return name;
+            }
+
+            set
+            {
+                name = value.ToUpper();
+            }
+        }
+
+        public string Author { 
+            get {
+                string masked = "";
+                for (int i = 0; i < author.Length; i++)
+                {
+                    masked += "*";
+                }
+                return masked;
+            }
+            
+            set => author = value; }
+
+
+        public int Year { get; set; }
+        public string Isbn { get; set; }
+        public string Publisher { get; set; }
+        public GenreType Genre { get; set; }
+        public bool Available { get {
+                return available;
+            }  }
+   
+
+        //methods:
+        //are the behavour of a Book
+        //syntax: <access modifier><return type (if any)><Name><list of parameters>
+        public void Borrow(string isbn)
+        {
+            if (Available)
+            {
+                available = false;
+            }
+        }
+
+        public int HowOldIsTheBook()
+        {
+            return DateTime.Now.Year - Year;
+        }
+
+
+    }
+}
---
+++ Week2_Classes/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week2_Classes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //class: is allowing us to define the entity e.g. Book
+            //object/instance: is allowing us to personalize the book
+            Book b; //declaration of the object
+            b = new Book(); //initialization of the object (creation of memory space that will hold data about b)
+                            //            Book b1 = new Book(); //this in one line;
+
+
+            Console.WriteLine("Input a name for the book");
+            b.Name = Console.ReadLine();
+
+            Console.WriteLine("Input an isbn for the book");
+            b.Isbn = Console.ReadLine();
+
+            Console.WriteLine("Input the year the book was published in");
+            try
+            {
+                b.Year = Convert.ToInt32(Console.ReadLine());
+            } 
+            catch 
+            {
+                Console.WriteLine("Invalid year input");
+            }
+
+
+            //approach 1
+           /* Console.WriteLine("1. Horror");
+            Console.WriteLine("2. Fiction");
+            Console.WriteLine("3. SciFi");
+           */
+
+            //approach 2
+            string[] namesInTheGenreSet = Enum.GetNames(typeof(GenreType));
+            for (int i = 0; i < namesInTheGenreSet.Length; i++)
+            {
+                Console.WriteLine($"{i+1}. {namesInTheGenreSet[i]}");
+            }
+            Console.WriteLine("Type the preferred Genre (number): ");
+            b.Genre = (GenreType) Convert.ToInt32(Console.ReadLine());
+
+            //to show back whatever the user has input on screen
+
+            Console.WriteLine($"Name: {b.Name} Isbn: {b.Isbn} Year: {b.Year} " +
+                $"Genre: { b.Genre } \n Book is {b.HowOldIsTheBook()} years old");
+            Console.ReadKey();
+
+
+
+        }
+    }
+}
